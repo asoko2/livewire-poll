0ad766a42fc2400cdc18029232e5568614c9598c
Anang Hariyanto N <sokoanang@gmail.com>
2024-05-13 16:01:37 +0700

    Finish Livewire Poll

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,7 +1,11 @@
 <div>
-    <form>
+    <form wire:submit.prevent="createPoll">
         <label>Poll Title</label>
         <input type="text" wire:model.live="title" />
+
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
 
         Current title: {{ $title }}
 
@@ -14,11 +18,16 @@
                 <div class="mb-4">
                     <label>Option {{ $index + 1 }}</label>
                     <div class="flex gap-2">
-                        <input type="text" wire:model="options.{{ $index }}" />
+                        <input type="text" wire:model.live="options.{{ $index }}" />
                         <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             @endforeach
         </div>
+
+        <button type="submit" class="btn">Create Poll</button>
     </form>
 </div>
